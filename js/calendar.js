229860e87f4c3adaa5225f438357cb36e31d301e
yeonhwalim <yeonhwalim03@gmail.com>
2023-02-15 06:04:37 +0900

    Completed calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,39 +1,81 @@
-const date = new Date();
+let date = new Date();
 
-const viewYear = date.getFullYear();
-const viewMonth = date.getMonth();
+const renderCalendar = () => {
+  const viewYear = date.getFullYear();
+  const viewMonth = date.getMonth();
 
-document.querySelector(".year-month").textContent = `${viewYear}년 ${
-  viewMonth + 1
-}월`;
+  document.querySelector(".year-month").textContent = `${viewYear}년 ${
+    viewMonth + 1
+  }월`;
 
-const prevLast = new Date(viewYear, viewMonth, 0);
-const thisLast = new Date(viewYear, viewMonth + 1, 0);
+  const prevLast = new Date(viewYear, viewMonth, 0);
+  const thisLast = new Date(viewYear, viewMonth + 1, 0);
 
-const PLDate = prevLast.getDate();
-const PLDay = prevLast.getDay();
+  const PLDate = prevLast.getDate();
+  const PLDay = prevLast.getDay();
 
-const TLDate = thisLast.getDate();
-const TLDay = thisLast.getDay();
+  const TLDate = thisLast.getDate();
+  const TLDay = thisLast.getDay();
 
-const prevDates = [];
-const thisDates = [...Array(TLDate + 1).keys()].slice(1);
-const nextDates = [];
+  const prevDates = [];
+  const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+  const nextDates = [];
 
-if (PLDay !== 6) {
-  for (let i = 0; i < PLDay + 1; i++) {
-    prevDates.unshift(PLDate - i);
+  // prevDates 계산
+  if (PLDay !== 6) {
+    for (let i = 0; i < PLDay + 1; i++) {
+      prevDates.unshift(PLDate - i);
+    }
   }
-}
 
-for (let i = 1; i < 7 - TLDay; i++) {
-  nextDates.push(i);
-}
+  // nextDates 계산
+  for (let i = 1; i < 7 - TLDay; i++) {
+    nextDates.push(i);
+  }
 
-const dates = prevDates.concat(thisDates, nextDates);
+  // Dates 합치기
+  const dates = prevDates.concat(thisDates, nextDates);
 
-dates.forEach((date, i) => {
-  dates[i] = `<div class="date">${date}</div>`;
-});
+  // Dates 정리
+  const firstDateIndex = dates.indexOf(1);
+  const lastDateIndex = dates.lastIndexOf(TLDate);
+  dates.forEach((date, i) => {
+    const condition =
+      i >= firstDateIndex && i < lastDateIndex + 1 ? "this" : "other";
 
-document.querySelector(".dates").innerHTML = dates.join("");
+    dates[
+      i
+    ] = `<div class="date"><span class="${condition}">${date}</span></div>`;
+  });
+
+  // Dates 그리기
+  document.querySelector(".dates").innerHTML = dates.join("");
+
+  const today = new Date();
+
+  if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+    for (let date of document.querySelectorAll(".this")) {
+      if (+date.innerText === today.getDate()) {
+        date.classList.add("today");
+        break;
+      }
+    }
+  }
+};
+
+renderCalendar();
+
+const prevMonth = () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+};
+
+const nextMonth = () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+};
+
+const goToday = () => {
+  date = new Date();
+  renderCalendar();
+};
