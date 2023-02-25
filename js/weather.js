2543bc0f1c92fd1ec37d03ce4c1649e686e15da7
yeonhwalim <yeonhwalim03@gmail.com>
2023-02-26 00:50:53 +0900

    Edit overall Frame

--- js/weather.js
+++ js/weather.js
@@ -1,17 +1,18 @@
+const weather = document.querySelector("#weather span:last-child");
+const city = document.querySelector("#weather span:first-child");
+
 const API_KEY = "9c109dfef15f06b7dbdb9f8a11c9a581";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   console.log("You live in", lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  /*fetch(url)
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:last-child");
-      const city = document.querySelector("#weather span:first-child");
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main}  ${data.main.temp} ° C`;
-    });*/
+    });
 }
 
 function onGeoError() {
