edf1ed88697e42e0da9eebed8b695f1e793f414d
yeonhwalim <yeonhwalim03@gmail.com>
2023-02-26 01:27:53 +0900

    Add quote

--- js/quotes.js
+++ js/quotes.js
@@ -1,50 +1,50 @@
 const quotes = [
   {
     quote: "Life is a series of building, testing, changing and iterating.",
-    author: "Lauren Mosenthal",
+    author: "-Lauren Mosenthal-",
   },
   {
     quote:
       "You will face many defeats in life, but never let yourself be defeated.",
-    author: "Maya Angelou",
+    author: "-Maya Angelou-",
   },
   {
     quote: "Momentum begets momentum, and the best way to start is to start.",
-    author: "Gil Penchina",
+    author: "-Gil Penchina-",
   },
   {
     quote:
       "Success usually comes to those who are too busy to be looking for it.",
-    author: "Henry David Thoreau",
+    author: "-Henry David Thoreau-",
   },
   {
     quote: "Do not be afraid to give up the good to go for the great.",
-    author: "John D.Rockefeller",
+    author: "-John D.Rockefeller-",
   },
   {
     quote:
       "The fastest way to change yourself is to hang out with people who are already the way you want to be.",
-    author: "Reid Hoffman",
+    author: "-Reid Hoffman-",
   },
   {
     quote:
       "Some people dream of success, while other people get up every morning and make it happen.",
-    author: "Wayne Huizenga",
+    author: "-Wayne Huizenga-",
   },
   {
     quote:
       "If you really want to do something, you’ll find a way. If you do not, you’ll find an excuse.",
-    author: "Jim Rohn",
+    author: "-Jim Rohn-",
   },
   {
     quote:
       "Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all.",
-    author: "Dale Carnegie",
+    author: "-Dale Carnegie-",
   },
   {
     quote:
       "Ability can take you to the top. But it takes character to keep you there.",
-    author: "Zig Ziglar",
+    author: "-Zig Ziglar-",
   },
 ];
 
